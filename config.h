b5d8b5ef989990bc516edffe1e22d242fd65502a
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-05-04 12:19:35 +0300

    Apply 'gridmode'

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 #include "movestack.c"
+#include "layouts.c"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -45,6 +46,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "HHH",      grid },
 };
 
 /* key definitions */
